58f59d363952ad8e9f399b7b0fe364f08e6a69cc
pesdesigner <pauloeduardo.dg@outlook.com>
2026-08-01 22:39:31 -0300

    OAuth2, JWT, password grant

---
+++ src/main/java/com/devsuperior/dscommerce/config/customgrant/CustomOAuth2ParameterNames.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscommerce.config.customgrant;
+
+public class CustomOAuth2ParameterNames {
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+    private CustomOAuth2ParameterNames() {
+        // Prevent instantiation
+    }
+}
